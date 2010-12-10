a9142ac13c94e75bb5053d3e7a741eceba95bdd3
Leo P <leo@slact.net>
2010-12-10 16:23:11 -0500

    actually import sha1 from sha1 or crypto modules for hashing index

--- lohm/index.lua
+++ lohm/index.lua
@@ -1,8 +1,24 @@
 module "lohm.index"
 local indexf = "lohm.index:%s:%s:%%s"
 
-local indices = {
-	hash = {
+local indices = {}
+
+--hash index: lookup by hashed value
+do
+	local s,  hash = pcall(require, "sha1")
+	if not s then
+		local s, crypto = pcall(require, "crypto")
+		if not s then 
+			error("Can't find a sha1 library (tried sha1 (lmd5) and crypto (LuaCrypto))")
+		else
+			local digest = (require "crypto.evp").digest
+			hash = function(input)
+				return digest("sha1", input)
+			end
+		end
+	end
+
+	indices.hash = {
 		update = function(self, redis, key, newval, oldval)
 			if(oldval~=nil) then
 				redis:srem(self:getKey(oldval), key)
@@ -16,7 +32,7 @@ local indices = {
 			return self.keyf:format(hash(val))
 		end
 	}
-}
+end
 
 function allIndices(sep)
 	local t = {}
