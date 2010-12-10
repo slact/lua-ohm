1f1ad253ea84ecd6daf88e6ee4de33493df48d69
Leo P <leo@slact.net>
2010-12-10 16:32:27 -0500

    initialization

--- lohm.lua
+++ lohm.lua
@@ -1,1 +1,13 @@
- 
+local Model = require "lohm.model"
+module ("lohm", function(self)
+	setmetatable(self, {__call = self.new})
+end)
+
+--arguments:
+	-- model (table): extra functions etc. belonging to the model
+	-- object (table): extra functions  etc. for objects
+	-- indices (table): attributes to index (and with what index, if given. defaults to Index:defaultIndex()
+
+function new(arg)
+	return Model.new(arg)
+end
