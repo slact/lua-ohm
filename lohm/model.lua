b4e467f7bdbd95ccb008593c7a3dff02ea8afbfc
Leo P <leo@slact.net>
2010-12-10 15:29:23 -0500

    potentially cleaner fromSort and fromSortDelayed implementations using coroutines (yet untested)

--- lohm/model.lua
+++ lohm/model.lua
@@ -18,40 +18,9 @@ local function getKey(self, id)
 	return modelcache[self]:getKey(id)
 end
 
-local modelmeta = { __index = {
-	find = function(self, arg)
-		if type(arg)=="table" then
-			return self:findByAttr(arg)
-		else
-			return self:findById(arg)
-		end
-	end,
-	
-	findById = function(self, id)
-		local key = getKey(self, id)
-		if not key then return 
-			nil, "Nothing to look for" 
-		end
-		local res, err = self.redis:hgetall(key)
-		if res then
-			return self:new(id, res)
-		else
-			return nil, "Not found."
-		end
-	end,
-
-	findByAttr = function(self, arg, limit, offset)
-		local indextable = {}
-        for attr, val in pairs(arg) do
-			local thisIndex = self.indices[attr]
-			assert(thisIndex, "model attribute " .. attr .. " isn't indexed. index it first, please.")
-			indextable[thisIndex]=val
-		end
-		return Index:lookup(indextable, limit, offset)
-		
-	end,
-	
-	fromSortDelayed = function(self, key, pattern, maxResults, offset, descending, lexicographic)
+local modelmeta
+do
+	local function fromSort_general(delay, self, key, pattern, maxResults, offset, descending, lexicographic)
 		local res, err = assert(self.redis:sort(key, {
 			by=pattern or "nosort", 
 			get="# GET " .. self:key("*"),  --oh the ugly!
@@ -59,38 +28,82 @@ local modelmeta = { __index = {
 			alpha = lexicographic or nil,
 			limit = maxResults and {offset or 0, maxResults}
 		}))
-		return function(res)
-			if res then
-				for i=0, #res, 2 do
-					res[i+1]=self:new(res[i], res[i+1])
-					table.remove(res, i)
-				end
+		if delay then
+			res, err = coroutine.yield()
+		end
+		if res then
+			for i=0, #res, 2 do
+				res[i+1]=self:new(res[i], res[i+1])
+				table.remove(res, i)
 			end
-		end, res
-	end, 
+		else
+			return nil, err or "unexpected thing cryptically happened..."
+		end
+	end
+	
+	
+	modelmeta = { __index = {
+		find = function(self, arg)
+			if type(arg)=="table" then
+				return self:findByAttr(arg)
+			else
+				return self:findById(arg)
+			end
+		end,
+		
+		findById = function(self, id)
+			local key = getKey(self, id)
+			if not key then return 
+				nil, "Nothing to look for" 
+			end
+			local res, err = self.redis:hgetall(key)
+			if res then
+				return self:new(id, res)
+			else
+				return nil, "Not found."
+			end
+		end,
 
-	fromSort = function(self, ...)
-		local processRes, res = self:fromSortDelayed(self, ...)
-		return assert(processRes(res))
-	end,
+		findByAttr = function(self, arg, limit, offset)
+			local indextable = {}
+			for attr, val in pairs(arg) do
+				local thisIndex = self.indices[attr]
+				assert(thisIndex, "model attribute " .. attr .. " isn't indexed. index it first, please.")
+				indextable[thisIndex]=val
+			end
+			return Index:lookup(indextable, limit, offset)
+			
+		end,
+		
+		fromSortDelayed = function(self, key, pattern, maxResults, offset, descending, lexicographic)
+			local wrapper = coroutine.wrap(fromSort_general)
+			assert(wrapper(true, self, key, pattern, maxResults, offset, descending, lexicographic))
+			return wrapper
+		end, 
 
-	fromSetDelayed = function(self, setKey, maxResults, offset, descending, lexicographic)
-		return self:fromSortDelayed(setKey, nil, maxResults, offset, descending, lexicographic)
-	end, 
+		fromSort = function(self, ...)
+			return fromSort_general(self, ...)
+		end,
 
-	fromSet = function(self, ...)
-		local processRes, res = assert(self:fromSetDelayed(self, ...))
-		return assert(processRes(res))
-	end, 
+		fromSetDelayed = function(self, setKey, maxResults, offset, descending, lexicographic)
+			local wrapper = coroutine.wrap(fromSort_general)
+			assert(wrapper(true, self, setKey, nil, maxResults, offset, descending, lexicographic))
+			return wrapper
+		end, 
 
-	key = function(self, id)
-		return keymaker(id)
-	end,
+		fromSet = function(self, ...)
+			return self:fromSetDelayed(self, ...)()
+		end, 
 
-	getModel = function(self, id)
-		return model[self]
-	end,
-}}
+		key = function(self, id)
+			return keymaker(id)
+		end,
+
+		getModel = function(self, id)
+			return model[self]
+		end,
+	}}
+end
 
 function new(arg, redisconn)
 	local model, object = arg.model or {}, arg.datum or arg.object or {}
